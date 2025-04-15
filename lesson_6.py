f0e063e07a67cfd96f9d16d414e1713b7d5089cf
alexiusbond <aleksey.bondarev@iaau.edu.kg>
2025-04-15 18:46:32 +0600

    lesson 6

--- lesson_6.py
+++ lesson_6.py
@@ -1,1 +1,23 @@
-# Here will be the code of lesson 6
+animals_list = ["cat", "cow", "dog"]
+fruits_list = ["apple", "pear", "banana", "mango", "kiwi"]
+
+for fruit in fruits_list:
+    print(fruit)
+
+for animal in animals_list:
+    print(animal)
+# O(F + A)
+
+for animal in fruits_list:
+    for fruit in fruits_list:
+        print(f'{animal} loves {fruit}')
+
+
+# O(A * F)
+
+def counter(number):
+    print(number)
+    if number > 0:
+        counter(number - 1)
+
+counter(3)
